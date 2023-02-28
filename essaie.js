f7d69d88e3c46a7597c22cd7aaf2e0e60a22e2fe
Romuald Moisan <moisan.romuald@gmail.com>
2023-02-28 17:41:38 +0100

    Changement 	modified:   essaie.js

--- essaie.js
+++ essaie.js
@@ -1,54 +1,37 @@
-const { timeStamp } = require('console');
-const fs = require('fs'); 
-let rawdata = fs.readFileSync('users.json','utf8');
-var args = process.argv;
-
-let att = 'country';
-
-
+const fs = require("fs"); 
+const process = require("process");
+let rawdata = fs.readFileSync("users.json");
 const data = JSON.parse(rawdata);
+const attribut = process.argv[2];
+const tab = [];
 
-
-let companies = [];
-
-function isIn(companytable, string){
-    for(let i = 0;i<companytable.length;i++){
-        if(companytable[i].Company == string){
+// fonction qui permet de savoir si string est dans le tableau
+function isIn(tableau, string,type){
+    for(let i = 0;i<tableau.length;i++){
+        if(tableau[i][type] == string){
             return true;
         }
     }
     return false;
 }
 
-function distinct(attribut)
-{
-    for(let i = 0; i < data.length; i++){
-        if(!isIn(companies,data[i][attribut])){
-            //Listing all the different companies
-            let obj = {Company:data[i][attribut], Count:1};
-            companies.push(obj);
-        }
-        else{   
-            let j = 0;
-            while(companies[j].Company != data[i][attribut]){ 
-                j++;
-            }
-            companies[j].Count++ ;
-        }
+//boucle qui permet de lister les différent attribut avec un compteur
+for(let i = 0; i < data.length; i++){
+    if(!isIn(tab,data[i][attribut],attribut)){
+        //Listing all the different tab
+        let obj = {[attribut]:data[i][attribut], counter:1};
+        tab.push(obj);
     }
-    
-    
-    for(let i = 0;i < companies.length;i++){ 
-        let tmp = companies[i];
-        let j = i-1;
-        while((j>=0) && (companies[j].Count < tmp.Count)){
-            companies[j+1] = companies[j];
-            j--;
+    else{   
+        let j = 0;
+        while(tab[j][attribut] != data[i][attribut]){ 
+            j++;
         }
-        companies[j+1] = tmp;
+        tab[j].counter++ ;
     }
-
-    console.log(companies);
 }
+// permet de trier les éléments par rapport au compteur
+tab.sort((a, b) => b.counter - a.counter);
+console.log(tab);
 
-distinct(att);
+
