6bc87ae5209af0900392ce7744e2d9dace7e8268
Romuald Moisan <moisan.romuald@gmail.com>
2023-03-01 15:56:00 +0100

    Include menu

--- essaie.js
+++ essaie.js
@@ -1,37 +1,57 @@
-const fs = require("fs"); 
-const process = require("process");
+// importation des modules
+const fs = require("fs");
+const inquirer = require("inquirer");
+// lecture du fichier "user.json"
 let rawdata = fs.readFileSync("users.json");
 const data = JSON.parse(rawdata);
-const attribut = process.argv[2];
-const tab = [];
 
 // fonction qui permet de savoir si string est dans le tableau
-function isIn(tableau, string,type){
-    for(let i = 0;i<tableau.length;i++){
-        if(tableau[i][type] == string){
+function isIn(tableau, string, type) {
+    for (let i = 0; i < tableau.length; i++) {
+        if (tableau[i][type] == string) {
             return true;
         }
     }
     return false;
 }
 
-//boucle qui permet de lister les différent attribut avec un compteur
-for(let i = 0; i < data.length; i++){
-    if(!isIn(tab,data[i][attribut],attribut)){
-        //Listing all the different tab
-        let obj = {[attribut]:data[i][attribut], counter:1};
-        tab.push(obj);
-    }
-    else{   
-        let j = 0;
-        while(tab[j][attribut] != data[i][attribut]){ 
-            j++;
+//fonction qui permet de lister les différent attribut avec un compteur
+function distinct(attribut) {
+    const tab = [];
+    for (let i = 0; i < data.length; i++) {
+        if (!isIn(tab, data[i][attribut], attribut)) {
+            //Listing all the different tab
+            let obj = { [attribut]: data[i][attribut], counter: 1 };
+            tab.push(obj);
         }
-        tab[j].counter++ ;
+        else {
+            let j = 0;
+            while (tab[j][attribut] != data[i][attribut]) {
+                j++;
+            }
+            tab[j].counter++;
+        }
     }
+    // permet de trier les éléments par rapport au compteur de façon décroissante
+    tab.sort((a, b) => b.counter - a.counter);
+    console.log(tab);
 }
-// permet de trier les éléments par rapport au compteur
-tab.sort((a, b) => b.counter - a.counter);
-console.log(tab);
+// fonction fait appelle à un menu
+function main() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "attribute",
+            message: "Select a category",
+            choices: ["country", "company", "quit"],
+        }
+    ]).then((answers) => {
+        if (answers.attribute == "quit") {
+            return;
+        }
+        console.table(distinct(answers.attribute));
+    });
+}
 
-
+//Lancement du programme main
+main();
